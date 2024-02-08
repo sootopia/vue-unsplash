3e982d5660ab993f50eefb0118b37ba4173f3275
sootopia <shpark7502@naver.com>
2024-02-08 20:08:44 +0900

    검색 목록 컴포넌트 추가

---
+++ src/components/search/ResultItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="result-card mb-6">
+    <figure class="result__card--thumbnail">
+      <a href="#none" class="block">
+        <img
+          class="w-full"
+          :src="data?.urls.thumb"
+          alt="data?.alt_description"
+          loading="lazy"
+        />
+      </a>
+    </figure>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    data: Object,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
