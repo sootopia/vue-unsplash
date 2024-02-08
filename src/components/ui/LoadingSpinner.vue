f78265ea06e5c5a6a480ebe0d24141a4e4b0804c
sootopia <shpark7502@naver.com>
2024-02-08 20:09:08 +0900

    로딩 스피너 컴포넌트 추가

---
+++ src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="spinner-container w-full flex items-center justify-center"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.spinner-container {
+}
+</style>
